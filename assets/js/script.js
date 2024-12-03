60e871fdf59d1a43e4ba7071557983475bbcb5d4
Creeford Sanchez <creewrk@gmail.com>
2024-12-02 19:33:42 -0800

    implement-convertation-with-ai

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,22 @@
 import { listen, select, style } from './data/utility.js';
 import { getResponse } from './ai/gemini.js';
 
-const container = select('.convertation');
+const convertation = select('.convertation');
 const input = select('textarea');
 const submitBtn = select('.fa-paper-plane');
 const output = select('p');
 
 listen(submitBtn, 'click', () => {
-  newMessage(true, input.value);
-  /*let question = input.value;
-  
-  getResponse(question).then((response) => {
-    output.innerText = response;
-  });
-
-  input.value = '';*/
+  if (validation()) {
+    let question = input.value;
+    newMessage(true, question);
+    input.value = '';
+    style(input, 'height', 'auto');
+    
+    getResponse(question).then((response) => { 
+      newMessage(false, response);
+    });
+  }
 });
 
 listen(input, 'input', () => {
@@ -27,7 +29,34 @@ listen(input, 'input', () => {
   validation();
 });
 
+function newMessage(isUser, response) {
+  const paragraph = document.createElement('p');
+  const icon = document.createElement('div');
+  const container = document.createElement('div');
 
+  addAttributes(isUser, response, paragraph, icon, container);
+  convertation.appendChild(container);
+}
+
+function addAttributes(isUser, response, paragraph, icon, container) {
+  icon.classList.add('icon');
+  paragraph.innerText = response;
+
+  if (isUser) {
+    container.classList.add('user-respond');
+    icon.classList.add('user-icon');
+
+    container.appendChild(paragraph);
+    container.appendChild(icon);
+  } else {
+    container.classList.add('ai-respond');
+    icon.classList.add('ai-icon');
+
+    container.appendChild(icon);
+    container.appendChild(paragraph);
+  }
+  
+}
 
 function validation() {
   if (input.value === '') {
@@ -38,4 +67,3 @@ function validation() {
   style(submitBtn, 'color', '#fff');
   return true;
 }
-
