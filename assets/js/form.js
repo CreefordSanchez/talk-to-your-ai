61159fa4cf2e886e617aefc74d21916ff54850c7
Creeford Sanchez <creewrk@gmail.com>
2024-12-05 09:07:03 -0800

    add-contact-btn-listen

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,3 +1,16 @@
 'use strict';
 
 import { listen, select, selectAll, style } from './data/utility.js';
+
+
+const submitBtn = select('.submit-contact');
+const contactBtn = select('.contact-btn');
+const contactContainer = select('.contact-form');
+
+listen(submitBtn, 'click', () => {
+  style(contactContainer, 'display', 'none');
+});
+
+listen(contactBtn, 'click', () => {
+  style(contactContainer, 'display', 'flex');
+});
